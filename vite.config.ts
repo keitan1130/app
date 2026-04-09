39a78bccc844f9959ca411bdf576d15045accb2f
keitan <keihawks1130@gmail.com>
2026-04-10 01:06:36 +0900

    cloudflare pagesに対応できるように

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/app/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL || '/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+  }
 })
